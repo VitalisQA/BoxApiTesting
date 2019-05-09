058e72aa387add1abbaa1fa3a51dcb9e6f6ddeff
Vitalii Fedorenko <vfedorenko@ekassa.com>
2019-05-09 12:17:19 +0300

    first commit

---
+++ src/main/java/BoxApiTesting/App.java
@@ -0,0 +1,13 @@
+package BoxApiTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
